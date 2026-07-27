53ec6e6cbd827ae2df77875b3327391b2b754e38
jazz <jscott85@student.cnm.edu>
2026-07-27 16:01:42 -0600

    Clean up unused imports and redundant annotations in `ShopController`.

--- server/src/main/java/edu/cnm/deepdive/coffeeshop/controller/ShopController.java
+++ server/src/main/java/edu/cnm/deepdive/coffeeshop/controller/ShopController.java
@@ -1,24 +1,19 @@
 package edu.cnm.deepdive.coffeeshop.controller;
 
-
 import edu.cnm.deepdive.coffeeshop.model.entity.Profile;
 import edu.cnm.deepdive.coffeeshop.model.entity.Shop;
-import edu.cnm.deepdive.coffeeshop.repository.ShopRepository;
 import edu.cnm.deepdive.coffeeshop.service.ShopService;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/shops")
