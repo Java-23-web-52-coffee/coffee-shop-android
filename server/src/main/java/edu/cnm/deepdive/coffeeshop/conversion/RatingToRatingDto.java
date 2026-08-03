533e33391e33ba39dbcb8671973a298c6cdf3e61
jazz <jscott85@student.cnm.edu>
2026-08-03 11:52:59 -0600

    Implement `RatingController` and `RatingService` for creating, updating, deleting, retrieving, and listing ratings. Add `RatingRequestDtoToRatingConverter` and `RatingToRatingDto` for DTO mapping. Extend `Visit` and `Interest` entities to manage associated `Rating` instances with cascading operations.

---
+++ server/src/main/java/edu/cnm/deepdive/coffeeshop/conversion/RatingToRatingDto.java
@@ -0,0 +1,21 @@
+package edu.cnm.deepdive.coffeeshop.conversion;
+
+import edu.cnm.deepdive.coffeeshop.model.dto.RatingDto;
+import edu.cnm.deepdive.coffeeshop.model.entity.Rating;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RatingToRatingDto implements Converter<Rating, RatingDto> {
+
+
+  @Override
+  public RatingDto convert(Rating source) {
+    RatingDto dto = new RatingDto();
+    dto.setValue(source.getValue());
+    dto.setVisitId(source.getVisit().getId());
+    dto.setInterestId(source.getInterest().getId());
+    return dto;
+  }
+
+}
